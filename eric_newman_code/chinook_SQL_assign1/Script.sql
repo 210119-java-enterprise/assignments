029432cf836af1f302117cbaa8d981a37062c93f
Eric Newman <googlnewman@gmail.com>
2021-01-29 00:00:08 -0600

    almost done saving progress on unions

--- eric_newman_code/chinook_SQL_assign1/Script.sql
+++ eric_newman_code/chinook_SQL_assign1/Script.sql
@@ -6,35 +6,29 @@
 
 --Part I – Working with an existing database
 
+--test calls
+select * FROM "Customer" c ;
+
+
 --Setting up Chinook
 --In this section you will begin the process of working with the Chinook database
 --Task – Set up the Chinook DB by executing the script found here
 --2.0 SQL Queries In this section you will be performing various queries against the Oracle Chinook database.
 --2.1 SELECT
 --Task – Select all records from the Employee table. 
-
 select * FROM "Employee" 
-
 --Task – Select all records from the Employee table where last name is King.
-
 select * FROM "Employee"
 	where "LastName" = 'King';
-
 --Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
-
 select * FROM "Employee"
 	where "FirstName" = 'Andrew' and "ReportsTo" is NULL;
-
 --2.2 ORDER BY
 --Task – Select all albums in album table and sort result set in descending order
-
 select * FROM "Album"
 	order by "AlbumId" desc; 
-
 --Task – Select first name from Customer and sort result set in ascending order
-
 select "FirstName" FROM "Customer" order by "FirstName" asc;
-
 --2.3 INSERT INTO
 --Task – Insert two new records into Genre table
 insert into "Genre" ("GenreId", "Name") values ('27', 'testrock'), ('28', 'morerock');
@@ -44,35 +38,85 @@ insert into "Employee" ("EmployeeId","FirstName", "LastName") values ('11', 'jef
 insert into "Customer" ("CustomerId","FirstName", "LastName", "Email") values ('61', 'ellen', 'newman', 'ellenmail'), ('62', 'jeff', 'newman', 'jeffmail');
 --2.4 UPDATE
 --Task – Update Aaron Mitchell in Customer table to Robert Walter
+update "Customer" set "FirstName" = 'Robert', "LastName" = 'Walter' where "FirstName" = 'Aaron' and "LastName" = 'Mitchell';
 --Task – Update name of artist “Creedence Clearwater Revival” to “CCR”
+update "Artist" set "Name" = 'CCR' where "Name" = 'Creedence Clearwater Revival';
 --2.5 LIKE
 --Task – Select all invoices with a billing address like “T”
+select * from "Invoice" where "BillingAddress" like 'T%';
 --2.6 BETWEEN
 --Task – Select all invoices that have a total between 15 and 50
+select * from "Invoice" where "Total" >= 15 and "Total" <= 50;
 --Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+select * from "Employee" where "HireDate" >= '2003-06-01 00:00:00' and "HireDate" <= '2004-03-01 00:00:00';
 --2.7 DELETE
 --Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
+-- alter table "Customer" delete constraints 
+alter table "Invoice" drop constraint "FK_InvoiceCustomerId";
+ALTER TABLE "Invoice" ADD CONSTRAINT "FK_InvoiceCustomerId" FOREIGN KEY ("CustomerId") REFERENCES "Customer"("CustomerId") on delete cascade;
+alter table "InvoiceLine" drop constraint "FK_InvoiceLineInvoiceId";
+ALTER TABLE "Invoice" ADD CONSTRAINT "FK_InvoiceLineInvoiceId" FOREIGN KEY ("CustomerId") REFERENCES "Customer"("CustomerId") on delete cascade;
 
-
+delete from "Customer" where "FirstName" = 'Robert' and "LastName" = 'Walter';
 --3.0 SQL Functions
 --In this section you will be using the PostGreSQL system functions, as well as your own functions, to perform various actions against the database
 --3.1 System Defined Functions
-
 --Task – Create a function that returns the current time.
+select current_time;
 --Task – create a function that returns the length of a mediatype from the mediatype table
+select "Name", length("Name") as "Media Length" from "MediaType";
 --3.2 System Defined Aggregate Functions
 --Task –Create a function that returns the average total of all invoices
+select avg("Total") as "Avg all invoices" from "Invoice";
 --Task – Create a function that returns the most expensive track
+select "Name" , "UnitPrice" from "Track" where "UnitPrice" = (select max("UnitPrice") from "Track");
 --3.3 User Defined Scalar Functions
 --Task – Create a function that returns the average price of invoice-line items in the invoice-line table
+--select * from "InvoiceLine";
+--select avg("UnitPrice") as "Average Price" from "InvoiceLine";
+--drop function getAvg();
+create or replace function getAvg()
+returns numeric
+as $$
+	declare 
+		average numeric;
+	begin
+		select avg("UnitPrice")
+		into average 
+		from "InvoiceLine";
+		return average;
+	end
+$$ 
+language plpgsql;
+select getAvg() as "average price";
 --3.4 User Defined Table Valued Functions
 --Task – Create a function that returns all employees who are born after 1968.
+--select * from "Employee";
+--drop function getYoung();
+create or replace function getYoung()
+returns setof "Employee"
+as $$
+	begin
+		return query
+			select * 
+			from "Employee" where "BirthDate" >= '1968-01-01 00:00:00'; 
+	end
+$$ language plpgsql;
+select getYoung();
 --5.0 JOINS
 --In this section you will be working with combining various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
 --5.1 INNER
 --Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+select * from "Customer";
+select * from "Invoice";
+
+select cus."FirstName", cus."LastName", ord."InvoiceId"
+from "Customer" cus -- finds the table identifier before resolving the select clause! 
+join "Invoice" ord -- keyword inner is always implied if nothing specified
+on cus."CustomerId" = ord."CustomerId";
 --5.2 OUTER
 --Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, last name, invoiceId, and total.
+
 --5.3 RIGHT
 --Task – Create a right join that joins album and artist specifying artist name and title.
 --5.4 CROSS
