1bbba990c08a8c63edae6a30427e03a520b57869
Gerts19 <gregory.gertsen@revature.net>
2021-01-28 21:01:16 -0800

    Half done

--- greg_gertsen_code/Chinook_Queries.sql
+++ greg_gertsen_code/Chinook_Queries.sql
@@ -2,38 +2,57 @@ Greg Gertsen
 
 --2.1 SELECT
 --Task – Select all records from the Employee table.
-
---select * from "Employee" e ;
+select * from "Employee";
 
 --Task – Select all records from the Employee table where last name is King.
 
-select * from e where "LastName"= "King";
+select * from "Employee" where "LastName" = 'King';
 
 --Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
 
+select * from "Employee" where "FirstName" = 'Andrew' and "ReportsTo" is null ;
 
 --2.2 ORDER BY
 --Task – Select all albums in album table and sort result set in descending order 
+select * from "Album" order by "AlbumId" desc;
 
 --Task – Select first name from Customer and sort result set in ascending order
+select "FirstName" from "Customer" order by  "FirstName"  asc;
 
 --2.3 INSERT INTO
---Task – Insert two new records into Genre table 
+--Task – Insert two new records into Genre table
+insert into "Genre" ("GenreId","Name") values 
+	(26,'Whale Sounds'),
+	(27, 'Cricket Sounds');
 
 --Task – Insert two new records into Employee table
+
+insert into "Employee" values 
+	(9, 'Greg', 'Donno', 'Boss', 3,'1983/12/8', '2020/5/1', '3303 NW 199th', 'Vancouver', 'WA', 'USA', '98642', '+1 (360) 234-2343', '+1 (360) 345-2343', 'greg@gmail.com'),
+	(10, 'Bill', 'Bar', 'Manager', 3,'1975/11/8', '2019/5/1', '3313 SW 200th', 'Portland', 'OR', 'USA', '98234', '+1 (360) 344-2334', '+1 (360) 334-2234', 'bill@gmail.com');
+
+
  
 --Task – Insert two new records into Customer table
+insert into "Customer" values (60,'Mike', 'Barker','534', 'Bourke Avenue','Hobart', 'NSW', 'Australia','2011','+61 (02) 9334 3623', 'mike.barker@yahoo.au','3'),
+								(61,'Bill', 'Taylor','431', 'Berry Street','Sidney', 'NSW', 'Australia','2018','+61 (02) 9123 3233', 'bill.taylor@yahoo.au','4');
 
 --2.4 UPDATE
 --Task – Update Aaron Mitchell in Customer table to Robert Walter
 
+update "Customer" set "FirstName"= 'Robert', "LastName" = 'Walter' where "FirstName"= 'Aaron' and "LastName" = 'Mitchell';
+
 --Task – Update name of artist “Creedence Clearwater Revival” to “CCR”
+
+update "Artist" set "Name"='CCR' where "Name" ='Creedence Clearwater Revival';
 
 --2.5 LIKE
 --Task – Select all invoices with a billing address like “T”
+select * from "Invoice" where "BillingAddress" like 'T%';
 
 --2.6 BETWEEN
 --Task – Select all invoices that have a total between 15 and 50​
+select * from "Invoice" where "Total" between 15 and 50;
 
 --Task – Select all employees hired between 1st​ of June 2003 and 1st​ of March 2004
 
