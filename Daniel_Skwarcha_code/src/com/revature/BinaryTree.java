02fac85668469f668b78e43c6a44b0f393e7932d
Daniel Skwarcha <daniel.skwarcha@revature.net>
2021-02-01 22:02:48 -0500

    Finished Binary Tree. Might implement more later.

--- Daniel_Skwarcha_code/src/com/revature/BinaryTree.java
+++ Daniel_Skwarcha_code/src/com/revature/BinaryTree.java
@@ -6,14 +6,14 @@ public class BinaryTree <T extends Comparable<? super T>> {
     private int size;
 
 
-    public void insert(T data) {
+    public void insert(Comparable<T> data) {
         if (data == null) {
             return;
         }
         root = traverseInsert(root, data);
     }
 
-    private Node traverseInsert(Node node, T data) {
+    private Node traverseInsert(Node node, Comparable<T> data) {
         if (node == null) { // base case for stopping recursion, if tree is empty or current node is null, then insert a new node
             node = new Node(data);
             size+=1;
@@ -31,54 +31,92 @@ public class BinaryTree <T extends Comparable<? super T>> {
         return node;
     }
 
-    public boolean contains(T data) {
-
+    public boolean contains(Comparable<T> data) {
+        if (data == null)
+        {
+            return false;
+        }
+        if(containsTraverse(root, data) == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
-    public void remove (T data) {
+    private Node containsTraverse(Node node, Comparable<T> data) {
+
+        // If the node is null or if the the value is found, return the node
+        if (node == null || node.data.compareTo(data) == 0)
+        {
+            return node;
+        }
+        else
+        {
+            // data in parameter is greater than data in current node, the node is equal to the node's right child called recursively
+            if (node.data.compareTo(data) < 0) {
+
+                node = containsTraverse(node.rightChild, data);
+            } else if (node.data.compareTo(data) > 0) { // data in parameter is less than data in node, the node is equal to the node's left child
+                node = containsTraverse(node.leftChild, data); // called recursively
+            }
+        }
+
+        // Needed to return the correct node in the end, else the recursion doesn't work
+        return node;
+    }
+
+    public void remove (Comparable<T> data) {
         if (data == null)
         {
             return;
-        }
+        } // if
 
         traverseRemove(root, data);
-    }
+    } // remove()
 
-    private void traverseRemove(Node node, T dataToRemove) {
+    private Node traverseRemove(Node node, Comparable<T> dataToRemove) {
 
-            if (node.data.compareTo(dataToRemove) == 0)
+        if (node == null)
+        {
+            return root;
+        } // if
+        // data in parameter is greater than data in current node
+        if (node.data.compareTo(dataToRemove) < 0) {
+            node.rightChild = traverseRemove(node.rightChild, dataToRemove);
+        } else if (node.data.compareTo(dataToRemove) > 0) { // data in parameter is less than data in node
+            node.leftChild = traverseRemove(node.leftChild, dataToRemove);
+        } // else if
+        else{
+            // if the left child is null, then the previous node's next child is this node's right child
+            if(node.leftChild == null)
             {
-                // This is a leaf (No children)
-                if(node.rightChild == null && node.leftChild == null)
-                {
-                    node = null;
-                    return;
-                }
-                else if (node.leftChild != null && node.rightChild == null)
-                {
-                    Node tempNode = node.leftChild;
-                    node.leftChild = null;
-                    node = tempNode;
-                }
-                else if (node.leftChild == null && node.rightChild != null)
-                {
-                    Node tempNode = node.rightChild;
-                    node.rightChild = null;
-                    node = tempNode;
-                }
-                else if (node.leftChild != null && node.rightChild != null) {
+                size-=1;
+                return node.rightChild;
 
-                }
-            }
-            // data to remove in parameter is greater than data in current node
-            else if (node.data.compareTo(data) < 0) {
-                node.rightChild = traverseInsert(node.rightChild, dataToRemove);
-            } else if (node.data.compareTo(data) > 0) { // data in parameter is less than data in node
-                node.leftChild = traverseInsert(node.leftChild, dataToRemove);
-            }
+            } // if
+            else if (node.rightChild == null) // If the right child is null, the previous node's next child is this node's left child
+            { size-=1;
+                return node.leftChild;
+            } // else if
+            size-=1;
+            Node tempNode = node.rightChild;
+            while(tempNode.leftChild != null)
+            {
+                node.data = tempNode.leftChild.data;
+                tempNode = tempNode.leftChild;
+            } // while
+            tempNode = null;
 
+            // Since the lowest value to the right of the binary tree was set to the node that was just removed, delete the node that has that
+            // value
+
+            node.rightChild = traverseRemove(node.rightChild, node.data);
+        } // else
         return node;
-    }
+    } // Node traverseRemove()
 
 
 
@@ -89,15 +127,15 @@ public class BinaryTree <T extends Comparable<? super T>> {
 
 
     private static class Node<T extends Comparable<? super T>> {
-        T data;
+        Comparable<T> data;
         Node<T> leftChild;
         Node<T> rightChild;
 
-        Node(T data) {
+        Node(Comparable<T> data) {
             this.data = data;
         }
 
-        Node(T data, Node<T> leftChild, Node<T> rightChild) {
+        Node(Comparable<T>  data, Node<T> leftChild, Node<T> rightChild) {
             this(data);
             this.leftChild = leftChild;
             this.rightChild = rightChild;
--- Daniel_Skwarcha_code/src/com/revature/Driver.java
+++ Daniel_Skwarcha_code/src/com/revature/Driver.java
@@ -1,4 +1,33 @@
 package com.revature;
 
 public class Driver {
+
+    public static void main(String [] args) {
+
+        BinaryTree myTree = new BinaryTree();
+
+        for(int i = 50; i <= 100; i++)
+        {
+            myTree.insert(i);
+            System.out.println("Added to myTree, value: " + i);
+            System.out.println("Size of tree" + myTree.size());
+        }
+
+        for(int i = 50; i>0; i--){
+            myTree.insert(i);
+            System.out.println("Added to myTree, value: " + i);
+            System.out.println("Size of tree" + myTree.size());
+        }
+
+        myTree.remove(75);
+        System.out.println("Removed 75");
+        System.out.println("Size of tree" + myTree.size());
+        myTree.remove(35);
+        System.out.println("Removed 35");
+        System.out.println("Size of tree" + myTree.size());
+
+        System.out.println("75 in is in the tree: " + myTree.contains(75));
+        System.out.println("35 is in the tree " + myTree.contains(35));
+
+    }
 }
