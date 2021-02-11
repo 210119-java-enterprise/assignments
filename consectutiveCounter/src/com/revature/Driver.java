bc5007b10ec149efa85b66d2eb8c49abad9c67f0
wills-designs <william3696@live.com>
2021-02-11 16:24:05 -0500

    blockQueue and semaphore assignments

---
+++ consectutiveCounter/src/com/revature/Driver.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        Integer sum = 0;
+        for (int i = 0; i <= 30; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
