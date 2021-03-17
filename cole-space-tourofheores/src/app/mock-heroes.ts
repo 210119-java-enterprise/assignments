48312fa7e27c220fd5fc8e382f64380c32208d45
Cole Space <cospace@unm.edu>
2021-03-16 22:11:18 -0600

    went through the angular tour of heroes

---
+++ cole-space-tourofheores/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'The Flash'},
+  {id: 12, name: 'The A.T.O.M'},
+  {id: 13, name: 'Black Canary'},
+  {id: 14, name: 'White Canary'},
+  {id: 15, name: 'Wild Dog'},
+  {id: 16, name: 'Black Lightning'},
+  {id: 17, name: 'Constantine'},
+  {id: 18, name: 'Heatwave'},
+  {id: 19, name: 'Captain Cold'},
+  {id: 20, name: 'Green Arrow'}
+];
