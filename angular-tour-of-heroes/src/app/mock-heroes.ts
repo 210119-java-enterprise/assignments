8826c180dd3148aa76218b5c738ee1f78274b1e2
Nate Gamble <nate.eli.gamble@gmail.com>
2021-03-17 23:08:10 -0400

    finished first half of angular tour of heroes demo

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Levi Ackermann'},
+    {id: 12, name: 'Captain America'},
+    {id: 13, name: 'Thanos'},
+    {id: 14, name: 'Groot'},
+    {id: 15, name: 'Batman'},
+    {id: 16, name: 'Wonder Woman'},
+    {id: 17, name: 'Mystique'},
+    {id: 18, name: 'Sonic'},
+    {id: 19, name: 'Link'},
+    {id: 20, name: 'Mythra'}
+];
