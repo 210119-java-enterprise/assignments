e40de4c8fa4dd54341aca2dc039904703a335fba
Gerts19 <gregory.gertsen@revature.net>
2021-03-18 21:35:39 -0700

    angular tour of heroes

---
+++ greg_gertsen_code/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Thunder Head' },
+  { id: 12, name: 'Death Claw' },
+  { id: 13, name: 'Hellious Bloodtooth' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Goblin Slayer' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Feyador Dethstoyeski' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
