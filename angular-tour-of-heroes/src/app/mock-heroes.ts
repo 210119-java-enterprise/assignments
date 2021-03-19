e4b3eaeb2b1f8f7898713d8e4312299240f9c129
Brian-Revature <brian.withrow@revature.net>
2021-03-19 01:28:28 -0400

    Literally spent 20 minutes after mistakenly git init-ing tour of heroes. Theres also an error somewhere in tour of heroes that Im having trouble identifying, potentially at step 4/5. Going back to edit tmmw.

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Lice' },
+  { id: 12, name: 'Marcos' },
+  { id: 13, name: 'Bomb Man' },
+  { id: 14, name: 'Celery-Tas' },
+  { id: 15, name: 'Magenta (Magenta)' },
+  { id: 16, name: 'ElastiGuy' },
+  { id: 17, name: 'DYNAMA!' },
+  { id: 18, name: 'Mr Dr Prof IQ' },
+  { id: 19, name: 'Lavaish' },
+  { id: 20, name: 'Whirly Birly' }
+];
